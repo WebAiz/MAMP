4a0dda4881ed7d3a03982a1bc379871c8aaf1f75
aizada <aizadaaziada@gmail.com>
2020-10-09 14:47:34 +0600

    MAMP

---
+++ first/index.php
@@ -0,0 +1,15 @@
+<title>Aizada Turarova</title>
+<h1> Hello from Web page</h1>
+<pre> This Pre tag makes content
+  separate as you want</pre>
+<p>
+<?php
+print hash('sha256', 'Aizada Turarova');
+
+$stuff = array('course' => 'PHP-Intro', 'topic' => 'Arrays');
+echo isset($stuff['section']);
+
+phpinfo();
+ ?>
+</p>
+<p>Another parapgraph</p>
